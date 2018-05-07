5a5b12c92bbeaccf88d32175c66c0cf641c0c45a
Hewitt VS <hewittvs@gmail.com>
2018-05-07 08:49:47 +0530

    Implementation of Notepad application in CSharp

---
+++ Notepad/Forms/FormReplace.cs
@@ -0,0 +1,89 @@
+﻿using NotepadCore;
+using NotepadCore.Functionality;
+using System;
+using System.Windows.Forms;
+
+namespace Notepad.Forms
+{
+    public partial class FormReplace : Form
+    {
+        FindNextSearch qry = new FindNextSearch();
+        public RichTextBox Editor;
+        public EditOperation editOpertion;
+
+        public FindNextSearch Qry
+        {
+            get
+            {
+                return qry;
+            }
+
+            set
+            {
+                qry = value;
+            }
+        }
+
+        public FormReplace()
+        {
+            InitializeComponent();
+        }
+
+        private void FormReplace_Load(object sender, EventArgs e)
+        {
+            DisableButtons();
+            rDown.Checked = true;
+        }
+
+       
+
+        private void DisableButtons()
+        {
+            if (txtFind.Text.Length == 0)
+            {
+                btnFind.Enabled =btnReplace.Enabled=btnReplaceAll.Enabled= false;
+            }else
+            {
+                btnFind.Enabled = btnReplace.Enabled = btnReplaceAll.Enabled = true;
+            }
+        }
+
+        private void txtFind_TextChanged(object sender, EventArgs e)
+        {
+            DisableButtons();
+        }
+        public void UpdateSearchQuery()
+        {
+            qry.SearchString = txtFind.Text;
+            qry.Direction = rUp.Checked ? "UP" : "DOWN";
+            qry.MatchCase = chkMatchCase.Checked;
+            qry.Content = Editor.Text;
+            qry.Position = Editor.SelectionStart;
+        }
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            UpdateSearchQuery();
+            FindNextResult result = editOpertion.FindNext(this.Qry);
+            if (result.SearchStatus)
+                this.Editor.Select(result.SelectionStart, txtFind.Text.Length);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            if (Editor.SelectionLength == 0)
+                btnFind.PerformClick();
+            else
+            Editor.SelectedText = txtReplace.Text;  
+        }
+
+        private void btnReplaceAll_Click(object sender, EventArgs e)
+        {
+            Editor.Text = Editor.Text.Replace(txtFind.Text, txtReplace.Text);
+        }
+    }
+}
---
+++ NotepadCore/EditOperation.cs
@@ -0,0 +1,90 @@
+﻿using NotepadCore.Functionality;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadCore
+{
+    public class EditOperation
+    {
+        private UndoRedoClass data;
+        private bool txtAreaTextChangeRequired=true;
+        public EditOperation()
+        {
+            data = new UndoRedoClass();
+        }
+
+        public bool TxtAreaTextChangeRequired
+        {
+            get
+            {
+                return txtAreaTextChangeRequired;
+            }
+
+            set
+            {
+                txtAreaTextChangeRequired = value;
+            }
+        }
+
+        public string DateTime_Now()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        public string UndoClicked()
+        {
+            TxtAreaTextChangeRequired = false;
+            return data.Undo();
+        }
+
+        public string RedoClicked()
+        {
+            TxtAreaTextChangeRequired = false;
+            return data.Redo();
+        }
+
+        public void Add_UndoRedo(string item)
+        {
+            data.AddItem(item);
+        }
+
+        public bool CanUndo()
+        {
+            return data.CanUndo();
+        }
+
+        public bool CanRedo()
+        {
+            return data.CanRedo();
+        }
+
+        public FindNextResult FindNext(FindNextSearch search)
+        {
+            FindNextResult result = new FindNextResult();
+            int position=-1;
+            StringComparison s = search.MatchCase ? StringComparison.CurrentCulture :
+                StringComparison.CurrentCultureIgnoreCase;
+            if (search.Direction == "UP")
+            {
+                position = search.Content.Substring(0, search.Position)
+                    .LastIndexOf(search.SearchString, s);
+                search.Success = position >= 0 ? true : false;
+                result.SearchStatus = search.Success;
+            }else
+            {
+                int start = search.Success ? search.Position + search.SearchString.Length :
+                    search.Position;
+                position = start + search.Content
+                    .Substring(start, search.Content.Length - start)
+                    .IndexOf(search.SearchString, s);
+                search.Success = position - start >= 0 ? true : false;
+                result.SearchStatus = search.Success;
+            }
+            result.SelectionStart = result.SearchStatus ? position : -1;
+            return result;
+        }
+    }
+}
