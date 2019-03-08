6bde09f3348b8d18987170a8d74cc3fcc766bafa
Hewitt VS <hewittvs@gmail.com>
2019-03-08 23:01:15 +0530

    Initial GUI Design for Notepad application

---
+++ Notepad/MainForm.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace Notepad
+{
+    public partial class MainForm : Form
+    {
+       
+        public MainForm()
+        {
+            InitializeComponent();
+            
+        }
+
+     
+    }
+}
