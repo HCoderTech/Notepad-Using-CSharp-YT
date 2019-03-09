8c1adc896eb241cb344d910d24dc5c00c155e5da
Hewitt VS <hewittvs@gmail.com>
2019-03-09 13:26:16 +0530

    Implementation of Open File Operation.

---
+++ Notepad/MainForm.cs
@@ -0,0 +1,89 @@
+﻿using NotepadCore;
+using System.Windows.Forms;
+using System;
+
+namespace Notepad
+{
+    public partial class MainForm : Form
+    {
+        FileOperation fileOperation;
+        public MainForm()
+        {
+            InitializeComponent();
+            fileOperation = new FileOperation();
+            fileOperation.InitializeNewFile();
+            this.Text = fileOperation.Filename;
+        }
+
+        private void newFileMenu_Click(object sender, System.EventArgs e)
+        {
+            //New File Menuitem Click
+            //If the file is saved Initialize new file status
+            //if the file is not saved ask to save the file
+            if (fileOperation.IsFileSaved)
+            {
+                //new file status
+                txtArea.Text = "";
+                fileOperation.InitializeNewFile();
+                UpdateView();
+            }else
+            {
+                DialogResult result=MessageBox.Show("Do you need save changes to " + fileOperation.Filename, "Notepad", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    if (fileOperation.Filename.Contains("Untitled"))
+                    {
+                        SaveFileDialog newFileSave = new SaveFileDialog();
+                        newFileSave.Filter = "Text(*.txt)|*.txt";
+                        if (newFileSave.ShowDialog() == DialogResult.OK)
+                        {
+                            fileOperation.SaveFile(newFileSave.FileName, txtArea.Lines);
+                            UpdateView();
+                        }
+                    }else
+                    {
+                        fileOperation.SaveFile(fileOperation.FileLocation, txtArea.Lines);
+                        UpdateView();
+                    }
+
+                }else if (result == DialogResult.No)
+                {
+                    txtArea.Text = "";
+                    fileOperation.InitializeNewFile();
+                    UpdateView();
+                }
+
+
+            }
+
+        }
+
+        private void UpdateView()
+        {
+            this.Text=!fileOperation.IsFileSaved?fileOperation.Filename+"*":fileOperation.Filename;
+        }
+
+        private void txtArea_TextChanged(object sender, EventArgs e)
+        {
+            fileOperation.IsFileSaved = false;
+            UpdateView();
+        }
+
+        private void openFileMenu_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text(*.txt)|*.txt";
+            openFile.InitialDirectory = "D:";
+            openFile.Title = "Open File";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                txtArea.TextChanged -= txtArea_TextChanged;
+                txtArea.Text = fileOperation.OpenFile(openFile.FileName);
+                txtArea.TextChanged += txtArea_TextChanged;
+                UpdateView();
+            }
+        }
+
+       
+    }
+}
---
+++ NotepadCore/FileOperation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadCore
+{
+    public class FileOperation
+    {
+        private string filename;
+        private bool isFileSaved;
+        private string fileLocation;
+
+        public string Filename
+        {
+            get
+            {
+                return filename;
+            }
+
+            set
+            {
+                filename = value;
+            }
+        }
+
+        public bool IsFileSaved
+        {
+            get
+            {
+                return isFileSaved;
+            }
+
+            set
+            {
+                isFileSaved = value;
+            }
+        }
+
+        public string FileLocation
+        {
+            get
+            {
+                return fileLocation;
+            }
+
+            set
+            {
+                fileLocation = value;
+            }
+        }
+
+        public void InitializeNewFile()
+        {
+            this.Filename = "Untitled.txt";
+            this.IsFileSaved = true;
+        }
+
+        public void SaveFile(string fileLocation, string[] lines)
+        {
+        
+        }
+
+        public string OpenFile(string fileLocation)
+        {
+            string content;
+            this.FileLocation = fileLocation;
+            Stream stream = File.Open(fileLocation, FileMode.Open, FileAccess.ReadWrite);
+            using (StreamReader streamReader = new StreamReader(stream))
+            {
+                content = streamReader.ReadToEnd();
+            }
+            UpdateFileStatus();
+            return content;
+        }
+
+        private void UpdateFileStatus()
+        {
+            string filename = FileLocation.Substring(FileLocation.LastIndexOf("\\") + 1);
+            this.Filename = filename;
+            this.IsFileSaved = true;
+        }
+    }
+}
