9fd8d2d7386edf2a94efe0aad55e5cc840812cb0
Hewitt VS <hewittvs@gmail.com>
2019-03-10 15:07:08 +0530

    Implementation of UndoRedo Operation Part1

---
+++ NotepadCore/EditOperation.cs
@@ -0,0 +1,20 @@
+﻿using NotepadCore.Functionality;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadCore
+{
+    public class EditOperation
+    {
+
+        public string DateTime_Now()
+        {
+            return DateTime.Now.ToString();
+        }
+
+
+    }
+}
