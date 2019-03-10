3432d16284a7a08dd8b9140c8fd801601cd65c52
Hewitt VS <hewittvs@gmail.com>
2019-03-10 13:32:34 +0530

    Implementation of Delete, DateTime and SelectAll operation.

---
+++ NotepadCore/EditOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadCore
+{
+    public class EditOperation
+    {
+        public string DateTime_Now()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
