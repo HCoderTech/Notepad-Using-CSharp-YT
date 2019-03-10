a0a77a4b3e4af4a3ba2440d0bba9d5cf1526dc32
Hewitt VS <hewittvs@gmail.com>
2019-03-10 15:23:47 +0530

    Implementation of UndoRedo Operation Part 2

---
+++ Notepad/MainForm.cs
@@ -0,0 +1,170 @@
+﻿using NotepadCore;
+using System.Windows.Forms;
+using System;
+
+namespace Notepad
+{
+    public partial class MainForm : Form
+    {
+        FileOperation fileOperation;
+        EditOperation editOperation;
+        public MainForm()
+        {
+            InitializeComponent();
+            fileOperation = new FileOperation();
+            editOperation = new EditOperation();
+            fileOperation.InitializeNewFile();
+            this.Text = fileOperation.Filename;
+        }
+
+        private void newFileMenu_Click(object sender, System.EventArgs e)
+        {
+            //New File Menuitem Click
+            //If the file is saved Initialize new file status
+            //if the file is not saved ask to save the file
+            if (fileOperation.IsFileSaved)
+            {
+                //new file status
+                txtArea.Text = "";
+                fileOperation.InitializeNewFile();
+                UpdateView();
+            }else
+            {
+                DialogResult result=MessageBox.Show("Do you need save changes to " + fileOperation.Filename, "Notepad", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    if (fileOperation.Filename.Contains("Untitled"))
+                    {
+                        SaveFileDialog newFileSave = new SaveFileDialog();
+                        newFileSave.Filter = "Text(*.txt)|*.txt";
+                        if (newFileSave.ShowDialog() == DialogResult.OK)
+                        {
+                            fileOperation.SaveFile(newFileSave.FileName, txtArea.Lines);
+                            UpdateView();
+                        }
+                    }else
+                    {
+                        fileOperation.SaveFile(fileOperation.FileLocation, txtArea.Lines);
+                        UpdateView();
+                    }
+
+                }else if (result == DialogResult.No)
+                {
+                    txtArea.Text = "";
+                    fileOperation.InitializeNewFile();
+                    UpdateView();
+                }
+
+
+            }
+
+        }
+
+        private void UpdateView()
+        {
+            this.Text=!fileOperation.IsFileSaved?fileOperation.Filename+"*":fileOperation.Filename;
+        }
+
+        private void txtArea_TextChanged(object sender, EventArgs e)
+        {
+            fileOperation.IsFileSaved = false;
+            UpdateView();
+        }
+
+        private void openFileMenu_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text(*.txt)|*.txt";
+            openFile.InitialDirectory = "D:";
+            openFile.Title = "Open File";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                txtArea.TextChanged -= txtArea_TextChanged;
+                txtArea.Text = fileOperation.OpenFile(openFile.FileName);
+                txtArea.TextChanged += txtArea_TextChanged;
+                UpdateView();
+            }
+        }
+
+        private void saveFileMenu_Click(object sender, EventArgs e)
+        {
+            if (!fileOperation.IsFileSaved)
+            {
+                if (!this.Text.Contains("Untitled.txt"))
+                {
+                    fileOperation.SaveFile(fileOperation.FileLocation, txtArea.Lines);
+                    UpdateView();
+                }else
+                {
+                    SaveFile();
+                }
+            }
+        }
+
+        private void SaveFile()
+        {
+            SaveFileDialog fileSave = new SaveFileDialog();
+            fileSave.Filter = "Text(*.txt)|*.txt";
+            if (fileSave.ShowDialog() == DialogResult.OK)
+            {
+                fileOperation.SaveFile(fileSave.FileName, txtArea.Lines);
+                UpdateView();
+            }
+        }
+
+        private void saveasFileMenu_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void exitFileMenu_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void editMenu_Click(object sender, EventArgs e)
+        {
+            cutEditMenu.Enabled = txtArea.SelectedText.Length > 0 ? true : false;
+            copyEditMenu.Enabled = txtArea.SelectedText.Length > 0 ? true : false;
+            pasteEditMenu.Enabled = Clipboard.GetDataObject().GetDataPresent(DataFormats.Text);
+        }
+
+        private void editMenu_MouseEnter(object sender, EventArgs e)
+        {
+            editMenu_Click(sender, e);
+        }
+
+        private void cutEditMenu_Click(object sender, EventArgs e)
+        {
+            txtArea.Cut();
+            pasteEditMenu.Enabled = true;
+        }
+
+        private void copyEditMenu_Click(object sender, EventArgs e)
+        {
+            txtArea.Copy();
+            pasteEditMenu.Enabled = true;
+        }
+
+        private void pasteEditMenu_Click(object sender, EventArgs e)
+        {
+            if(Clipboard.GetDataObject().GetDataPresent(DataFormats.Text))
+            txtArea.Paste();
+        }
+
+        private void selectallEditMenu_Click(object sender, EventArgs e)
+        {
+            txtArea.SelectAll();
+        }
+
+        private void deleteEditMenu_Click(object sender, EventArgs e)
+        {
+            txtArea.Text = txtArea.Text.Remove(txtArea.SelectionStart, txtArea.SelectionLength);
+        }
+
+        private void timedateEditMenu_Click(object sender, EventArgs e)
+        {
+            txtArea.Text = txtArea.Text.Insert(txtArea.SelectionStart, editOperation.DateTime_Now());
+        }
+    }
+}
---
+++ NotepadCore/EditOperation.cs
@@ -0,0 +1,64 @@
+﻿using NotepadCore.Functionality;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadCore
+{
+    public class EditOperation
+    {
+        private UndoRedoClass data;
+        private bool txtAreaTextChangeRequired;
+        public EditOperation()
+        {
+            data = new UndoRedoClass();
+        }
+
+        public bool TxtAreaTextChangeRequired
+        {
+            get
+            {
+                return txtAreaTextChangeRequired;
+            }
+
+            set
+            {
+                txtAreaTextChangeRequired = value;
+            }
+        }
+
+        public string DateTime_Now()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        public string UndoClicked()
+        {
+            TxtAreaTextChangeRequired = false;
+            return data.Undo();
+        }
+
+        public string RedoClicked()
+        {
+            TxtAreaTextChangeRequired = false;
+            return data.Redo();
+        }
+
+        public void Add_UndoRedo(string item)
+        {
+            data.AddItem(item);
+        }
+
+        public bool CanUndo()
+        {
+            return data.CanUndo();
+        }
+
+        public bool CanRedo()
+        {
+            return data.CanRedo();
+        }
+    }
+}
