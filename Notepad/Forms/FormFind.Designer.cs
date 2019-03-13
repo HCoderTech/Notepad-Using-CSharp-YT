6f74443525607d56430aa46ee32bd547cdadbd65
Hewitt VS <hewittvs@gmail.com>
2019-03-13 09:04:25 +0530

    Implementation of Finding a String Operation Part2

---
+++ Notepad/Forms/FormFind.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Notepad.Forms
+{
+    partial class FormFind
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.btnFindNext = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.gDirection = new System.Windows.Forms.GroupBox();
+            this.oDown = new System.Windows.Forms.RadioButton();
+            this.oUp = new System.Windows.Forms.RadioButton();
+            this.chkMatchCase = new System.Windows.Forms.CheckBox();
+            this.gDirection.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Find what:";
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(108, 31);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(160, 20);
+            this.txtFind.TabIndex = 1;
+            
+            // 
+            // btnFindNext
+            // 
+            this.btnFindNext.Location = new System.Drawing.Point(302, 29);
+            this.btnFindNext.Name = "btnFindNext";
+            this.btnFindNext.Size = new System.Drawing.Size(84, 23);
+            this.btnFindNext.TabIndex = 2;
+            this.btnFindNext.Text = "Find Next";
+            this.btnFindNext.UseVisualStyleBackColor = true;
+            
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(302, 67);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(84, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // gDirection
+            // 
+            this.gDirection.Controls.Add(this.oDown);
+            this.gDirection.Controls.Add(this.oUp);
+            this.gDirection.Location = new System.Drawing.Point(151, 58);
+            this.gDirection.Name = "gDirection";
+            this.gDirection.Size = new System.Drawing.Size(136, 57);
+            this.gDirection.TabIndex = 4;
+            this.gDirection.TabStop = false;
+            this.gDirection.Text = "Direction";
+            // 
+            // oDown
+            // 
+            this.oDown.AutoSize = true;
+            this.oDown.Location = new System.Drawing.Point(64, 20);
+            this.oDown.Name = "oDown";
+            this.oDown.Size = new System.Drawing.Size(53, 17);
+            this.oDown.TabIndex = 1;
+            this.oDown.TabStop = true;
+            this.oDown.Text = "Down";
+            this.oDown.UseVisualStyleBackColor = true;
+            // 
+            // oUp
+            //
+            
+            this.oUp.AutoSize = true;
+            this.oUp.Location = new System.Drawing.Point(7, 20);
+            this.oUp.Name = "oUp";
+            this.oUp.Size = new System.Drawing.Size(39, 17);
+            this.oUp.TabIndex = 0;
+            this.oUp.TabStop = true;
+            this.oUp.Text = "Up";
+            this.oUp.UseVisualStyleBackColor = true;
+            // 
+            // chkMatchCase
+            // 
+            this.chkMatchCase.AutoSize = true;
+            this.chkMatchCase.Location = new System.Drawing.Point(13, 97);
+            this.chkMatchCase.Name = "chkMatchCase";
+            this.chkMatchCase.Size = new System.Drawing.Size(83, 17);
+            this.chkMatchCase.TabIndex = 5;
+            this.chkMatchCase.Text = "Match Case";
+            this.chkMatchCase.UseVisualStyleBackColor = true;
+           
+            // 
+            // FormFind
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 141);
+            this.Controls.Add(this.chkMatchCase);
+            this.Controls.Add(this.gDirection);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnFindNext);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.label1);
+            this.Name = "FormFind";
+            this.Text = "Find";
+           
+            this.gDirection.ResumeLayout(false);
+            this.gDirection.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.Button btnFindNext;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.GroupBox gDirection;
+        private System.Windows.Forms.RadioButton oDown;
+        private System.Windows.Forms.RadioButton oUp;
+        private System.Windows.Forms.CheckBox chkMatchCase;
+    }
+}
---
+++ Notepad/MainForm.cs
@@ -0,0 +1,237 @@
+﻿using NotepadCore;
+using System.Windows.Forms;
+using System;
+using Microsoft.VisualBasic;
+
+namespace Notepad
+{
+    public partial class MainForm : Form
+    {
+        FileOperation fileOperation;
+        EditOperation editOperation;
+        Timer timer;
+
+        public EditOperation EditOperation { get { return editOperation; } }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            fileOperation = new FileOperation();
+            editOperation = new EditOperation();
+            fileOperation.InitializeNewFile();
+            this.Text = fileOperation.Filename;
+            timer = new Timer();
+            timer.Tick += Mytimer_Tick;
+            timer.Interval = 500;
+            //time t1 --> My name
+            //time t2 --> My name is Sara
+        }
+
+        private void Mytimer_Tick(object sender,EventArgs e)
+        {
+            timer.Stop();
+            editOperation.Add_UndoRedo(txtArea.Text);
+            UpdateView();
+        }
+        private void newFileMenu_Click(object sender, System.EventArgs e)
+        {
+            //New File Menuitem Click
+            //If the file is saved Initialize new file status
+            //if the file is not saved ask to save the file
+            if (fileOperation.IsFileSaved)
+            {
+                //new file status
+                txtArea.Text = "";
+                fileOperation.InitializeNewFile();
+                UpdateView();
+            }else
+            {
+                DialogResult result=MessageBox.Show("Do you need save changes to " + fileOperation.Filename, "Notepad", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    if (fileOperation.Filename.Contains("Untitled"))
+                    {
+                        SaveFileDialog newFileSave = new SaveFileDialog();
+                        newFileSave.Filter = "Text(*.txt)|*.txt";
+                        if (newFileSave.ShowDialog() == DialogResult.OK)
+                        {
+                            fileOperation.SaveFile(newFileSave.FileName, txtArea.Lines);
+                            UpdateView();
+                        }
+                    }else
+                    {
+                        fileOperation.SaveFile(fileOperation.FileLocation, txtArea.Lines);
+                        UpdateView();
+                    }
+
+                }else if (result == DialogResult.No)
+                {
+                    txtArea.Text = "";
+                    fileOperation.InitializeNewFile();
+                    UpdateView();
+                }
+
+
+            }
+
+        }
+
+        private void UpdateView()
+        {
+            this.Text=!fileOperation.IsFileSaved?fileOperation.Filename+"*":fileOperation.Filename;
+            undoEditMenu.Enabled = editOperation.CanUndo() ? true : false;
+            redoEditMenu.Enabled = editOperation.CanRedo() ? true : false;
+        }
+
+        private void txtArea_TextChanged(object sender, EventArgs e)
+        {
+            fileOperation.IsFileSaved = false;
+            if (editOperation.TxtAreaTextChangeRequired)
+            {
+                timer.Start();
+            }else
+            {
+                editOperation.TxtAreaTextChangeRequired = false;
+            }
+            UpdateView();
+        }
+
+        private void openFileMenu_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text(*.txt)|*.txt";
+            openFile.InitialDirectory = "D:";
+            openFile.Title = "Open File";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                txtArea.TextChanged -= txtArea_TextChanged;
+                txtArea.Text = fileOperation.OpenFile(openFile.FileName);
+                txtArea.TextChanged += txtArea_TextChanged;
+                UpdateView();
+            }
+        }
+
+        private void saveFileMenu_Click(object sender, EventArgs e)
+        {
+            if (!fileOperation.IsFileSaved)
+            {
+                if (!this.Text.Contains("Untitled.txt"))
+                {
+                    fileOperation.SaveFile(fileOperation.FileLocation, txtArea.Lines);
+                    UpdateView();
+                }else
+                {
+                    SaveFile();
+                }
+            }
+        }
+
+        private void SaveFile()
+        {
+            SaveFileDialog fileSave = new SaveFileDialog();
+            fileSave.Filter = "Text(*.txt)|*.txt";
+            if (fileSave.ShowDialog() == DialogResult.OK)
+            {
+                fileOperation.SaveFile(fileSave.FileName, txtArea.Lines);
+                UpdateView();
+            }
+        }
+
+        private void saveasFileMenu_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void exitFileMenu_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void editMenu_Click(object sender, EventArgs e)
+        {
+            cutEditMenu.Enabled = txtArea.SelectedText.Length > 0 ? true : false;
+            copyEditMenu.Enabled = txtArea.SelectedText.Length > 0 ? true : false;
+            pasteEditMenu.Enabled = Clipboard.GetDataObject().GetDataPresent(DataFormats.Text);
+        }
+
+        private void editMenu_MouseEnter(object sender, EventArgs e)
+        {
+            editMenu_Click(sender, e);
+        }
+
+        private void cutEditMenu_Click(object sender, EventArgs e)
+        {
+            txtArea.Cut();
+            pasteEditMenu.Enabled = true;
+        }
+
+        private void copyEditMenu_Click(object sender, EventArgs e)
+        {
+            txtArea.Copy();
+            pasteEditMenu.Enabled = true;
+        }
+
+        private void pasteEditMenu_Click(object sender, EventArgs e)
+        {
+            if(Clipboard.GetDataObject().GetDataPresent(DataFormats.Text))
+            txtArea.Paste();
+        }
+
+        private void selectallEditMenu_Click(object sender, EventArgs e)
+        {
+            txtArea.SelectAll();
+        }
+
+        private void deleteEditMenu_Click(object sender, EventArgs e)
+        {
+            txtArea.Text = txtArea.Text.Remove(txtArea.SelectionStart, txtArea.SelectionLength);
+        }
+
+        private void timedateEditMenu_Click(object sender, EventArgs e)
+        {
+            txtArea.Text = txtArea.Text.Insert(txtArea.SelectionStart, editOperation.DateTime_Now());
+        }
+
+        private void undoEditMenu_Click(object sender, EventArgs e)
+        {
+            txtArea.Text = editOperation.UndoClicked();
+            UpdateView();
+        }
+
+        private void redoEditMenu_Click(object sender, EventArgs e)
+        {
+            txtArea.Text = editOperation.RedoClicked();
+            UpdateView();
+        }
+
+        private void gotoEditMenu_Click(object sender, EventArgs e)
+        {
+            string input = Interaction.InputBox("Line Number", "Go To", "1");
+            try
+            {
+                int line = Convert.ToInt32(input);
+                if (line > txtArea.Lines.Length)
+                {
+                    MessageBox.Show("Total lines in the file is " + txtArea.Lines.Length, "Can't Reach",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }else
+                {
+                    string[] lines = txtArea.Lines;
+                    int len = 0;
+                    for(int i = 0; i < line - 1; i++)
+                    {
+                        len = len + lines[i].Length+1;
+                    }
+                    txtArea.Focus();
+                    txtArea.Select(len,0);
+                }
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Enter a valid Integer", "Wrong Input", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+    }
+}
