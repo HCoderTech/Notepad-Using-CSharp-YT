2f4ef0573d4a733e03eeecef4f61e45dc38996c9
Hewitt VS <hewittvs@gmail.com>
2019-03-13 08:34:33 +0530

    Implementation of Finding A String Operation Part1

---
+++ Notepad/Forms/FormFind.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace Notepad.Forms
+{
+    public partial class FormFind : Form
+    {
+      
+        public FormFind(MainForm mainForm)
+        {
+            InitializeComponent();
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnFindNext_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void UpdateSearchQuery()
+        {
+           
+        }
+
+        private void FormFind_FormClosing(object sender, FormClosingEventArgs e)
+        {
+          
+        }
+
+        private void oUp_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void chkMatchCase_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
